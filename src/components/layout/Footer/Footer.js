b79edfa3180de5dc1eb8b235f8c4daa94f9f8214
Andreas Stopat <dev@demonsgalore.de>
2019-09-27 18:37:48 +0200

    install FontAwesome

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
 
 const Footer = () => {
   return (
     <footer>
       <h1>FOOTER</h1>
+      <FontAwesomeIcon icon={faCopyright} size="1x" /> {new Date().getFullYear()}
     </footer>
   );
 };
